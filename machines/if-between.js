f3ad0d8e76b202766f23e04a9cd657f07777e2cd
Mike McNeil <mike@balderdash.co>
2015-05-04 17:47:51 -0500

    Trigger error exit if inconsistent input values for min/max are provided.

--- machines/if-between.js
+++ machines/if-between.js
@@ -54,6 +54,11 @@ module.exports = {
   fn: function (inputs, exits) {
     var _ = require('lodash');
 
+    // if inputs are inconsistent with expectations, bail out w/ `error`
+    if (inputs.min > inputs.max) {
+      return exits.error(new Error('The configured value of the `min` input must be <= that of `max`.'));
+    }
+
     // because lodash's `inRange` is only inclusive on the bottom side,
     // we'll make it inclusive here by adding an additional check:
     if (_.inRange(inputs.value, inputs.min, inputs.max) || (inputs.max === inputs.value)){
