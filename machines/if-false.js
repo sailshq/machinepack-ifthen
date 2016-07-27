cfdc2f8c317c3dc39aa07deb6f6341b2f6214c1c
Scott Gress <scottmgress@gmail.com>
2016-07-27 18:06:57 -0500

    Added "if-true" and "if-false" machines

---
+++ machines/if-false.js
@@ -0,0 +1,58 @@
+module.exports = {
+
+
+  friendlyName: 'If not...',
+
+
+  description: 'Determine whether the value is "falsey".',
+
+
+  extendedDescription: 'The \'success\' exit will be triggered for any "falsey" value (`0`, `\'\'`, `null`, `false`, `NaN` and `undefined`).',
+
+
+  sideEffects: 'cacheable',
+
+
+  sync: true,
+
+
+  moreInfoUrl: 'https://www.sitepoint.com/javascript-truthy-falsy/',
+
+
+  inputs: {
+
+    value: {
+      description: 'The value to check.',
+      extendedDescription: 'A value of any type may be provided.  If it is "falsey", the \'success\' exit will be triggered.  Otherwise the \'otherwise\' exit will be triggered.',
+      example: '==='
+    }
+
+  },
+
+
+  exits: {
+
+    success: {
+      description: 'The value is "falsey".',
+    },
+
+    otherwise: {
+      friendlyName: 'Else',
+      description: 'The value is "truthy".'
+    }
+
+  },
+
+
+  fn: function(inputs, exits, env) {
+
+    // If the input value is falsey, trigger the `success` exit.
+    if (!inputs.value) {
+      return exits.success();
+    }
+
+    // Otherwise trigger the `otherwise` exit.
+    return exits.otherwise();
+  }
+
+};
---
+++ machines/if-true.js
@@ -0,0 +1,58 @@
+module.exports = {
+
+
+  friendlyName: 'If...',
+
+
+  description: 'Determine whether the value is "truthy".',
+
+
+  extendedDescription: 'The \'success\' exit will be triggered for any "truthy" value (everything except `0`, `\'\'`, `null`, `false`, `NaN` and `undefined`).',
+
+
+  sideEffects: 'cacheable',
+
+
+  sync: true,
+
+
+  moreInfoUrl: 'https://www.sitepoint.com/javascript-truthy-falsy/',
+
+
+  inputs: {
+
+    value: {
+      description: 'The value to check.',
+      extendedDescription: 'A value of any type may be provided.  If it is "truthy", the \'success\' exit will be triggered.  Otherwise the \'otherwise\' exit will be triggered.',
+      example: '==='
+    }
+
+  },
+
+
+  exits: {
+
+    success: {
+      description: 'The value is "truthy".',
+    },
+
+    otherwise: {
+      friendlyName: 'Else',
+      description: 'The value is "falsey".'
+    }
+
+  },
+
+
+  fn: function(inputs, exits, env) {
+
+    // If the input value is truthy, trigger the `success` exit.
+    if (inputs.value) {
+      return exits.success();
+    }
+
+    // Otherwise trigger the `otherwise` exit.
+    return exits.otherwise();
+  }
+
+};
